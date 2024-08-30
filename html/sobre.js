8658e4ca0b61cc898da1e2d943529d1d21e02627
ivyesilva08 <mariafernandadasiva0801@gmail.com>
2024-08-29 22:01:33 -0300

    sobre update

---
+++ html/sobre.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var h1 = document.getElementByid('h1');
+    
+    // Adiciona uma classe para animar o texto
+    texto.classList.add('animar');
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var p = document.getElementByid('p');
+    
+    // Adiciona uma classe para animar o texto
+    texto.classList.add('animar');
+  });
